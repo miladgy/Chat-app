991aea635a660e1660155f8272d1c5c6e3af5753
Milad Ghadami Yazdi <milad.ghadami@appliedtechnology.se>
2020-01-14 17:55:48 +0100

    send and receive messages and user info

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,13 +8,15 @@ let socket = io(ENDPOINT);
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
     const { name } = queryString.parse(location.search);
     socket = io.connect();
 
     setName(name);
-    socket.emit("signin", { name }, ({ err }) => {
-      console.log(err);
+    socket.emit("signin", { name }, (error) => {
+      console.log(error);
     });
     return () => {
       socket.emit('disconnect');
@@ -22,9 +24,31 @@ const Chat = ({ location }) => {
     }
     
   }, [ENDPOINT, location.search]);
+
+useEffect(() => {
+  socket.on('message', (message) => {
+    console.log('do i come in here')
+    setMessages([...messages, message]);
+  })
+}, [messages])
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  if (message){
+    socket.emit('sendMessage', message, () => setMessage(''))
+  }
+}
+console.log('message', message)
+console.log('messagez', messages)
   return (
     <div className="chatOuterContainer">
-      <div className="chatInnerContainer"></div>
+      <div className="chatInnerContainer">
+        <input 
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
+        />
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -9,15 +9,31 @@ const router = require("./router");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const {addUser, removeUser, getUser} = require("./users.js") 
-
+const { addUser, removeUser, getUser } = require("./users.js");
 
 io.on("connection", socket => {
   console.log("a user connected");
 
-  socket.on('signin', ({name}, cb) => {
-    
+  socket.on("signin", ({ name }, cb) => {
+    const { error, user } = addUser({ id: socket.id, name });
+    if (error) {
+      return cb(error);
+    }
+    socket.emit("message", {
+      user: "admin",
+      welcomeText: `Welcome to the chat room ${user.name}.`
+    });
+    socket.broadcast.emit("message", {
+      user: "admin",
+      text: `${user.name} has joined our chat.`
+    });
+    cb();
   });
+  socket.on('sendMessage', (message, cb) => {
+    const user = getUser(socket.id)
+    io.emit('message', {user: user.name, text: message})
+    cb();
+  })
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
